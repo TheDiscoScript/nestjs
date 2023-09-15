f52e7dddaab6480c6e2cd1becdeda828ca053d33
TheDiscoScript <wollmada@gmail.com>
2023-09-15 11:15:35 +0200

    feat(system): previous commit enum was way too long

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [2, "always", ["system", "useful-implementations"]],
+    "scope-enum": [2, "always", ["system", "nx-app", "nx-lib"]],
     "scope-empty": [2, "never"],
     "subject-empty": [2, "never"],
   },
